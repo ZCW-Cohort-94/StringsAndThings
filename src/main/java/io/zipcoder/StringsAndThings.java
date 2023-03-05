111269d049ded706172923a20b02c047cda572d4
Robert <robert@Zipcoders-MacBook-Pro-60.local>
2023-03-05 00:23:11 -0500

    Added String Removal code

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,6 +1,8 @@
 package io.zipcoder;
 
 
+import java.sql.SQLOutput;
+
 /**
  * @author tariq
  */
@@ -14,9 +16,19 @@ public class StringsAndThings {
      *           countYZ("day fez"); // Should return 2
      *           countYZ("day fyyyz"); // Should return 2
      */
-    public Integer countYZ(String input){
-        return null;
+
+    public Integer countYZ(String input) {
+        int sum = 0;
+        String [] words = input.split(" ");
+
+        for(int i = 0; i < words.length; i++) {
+            if(input.endsWith("y") || input.endsWith("z")) {
+                sum++;
+            }
+        }
+        return sum;
     }
+
 
     /**
      * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
@@ -28,7 +40,7 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+        return base.replace(remove, "");
     }
 
     /**
@@ -40,8 +52,9 @@ public class StringsAndThings {
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
     public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
-    }
+
+
+    return null; }
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
@@ -51,8 +64,10 @@ public class StringsAndThings {
      *           gHappy("xxggyygxx") // Should return  false
      */
     public Boolean gIsHappy(String input){
+
         return null;
     }
+
 
 
     /**
